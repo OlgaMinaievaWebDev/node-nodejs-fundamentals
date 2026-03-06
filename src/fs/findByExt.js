a594b7d687d998989b370e8535120975b9202f08
Olga <minaeva9@gmail.com>
2026-03-06 17:21:10 -0500

    feat:solve findByExt task

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,32 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const findByExt = async () => {
   // Write your code here
   // Recursively find all files with specific extension
   // Parse --ext CLI argument (default: .txt)
+
+  const workspacePath = path.resolve('workspace');
+
+  const extIndex = process.argv.indexOf('--ext');
+  const targetExt = extIndex !== -1 ? process.argv[extIndex + 1] : '.txt';
+
+  async function scanDir(currentDir) {
+    const data = await fs.readdir(currentDir, { withFileTypes: true });
+
+    for (const entry of data) {
+      const absPath = path.join(currentDir, entry.name);
+      if (entry.isDirectory()) {
+        await scanDir(absPath);
+      } else if (entry.isFile()) {
+        const extension = path.extname(entry.name);
+        if (extension === targetExt) {
+          console.log(path.relative(workspacePath, absPath));
+        }
+      }
+    }
+  }
+  await scanDir(workspacePath);
 };
 
 await findByExt();
