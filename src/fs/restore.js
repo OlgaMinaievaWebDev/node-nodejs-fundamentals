a68861606cfca242416d284cadf918b915c2f4f6
Olga <minaeva9@gmail.com>
2026-03-06 16:50:26 -0500

    feat:solve restore task

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,33 @@
+import { mkdir } from 'node:fs';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 const restore = async () => {
   // Write your code here
   // Read snapshot.json
   // Treat snapshot.rootPath as metadata only
   // Recreate directory/file structure in workspace_restored
+  try {
+    const data = await fs.readFile('snapshot.json', 'utf-8');
+    const snapshot = JSON.parse(data);
+
+    const destDir = path.resolve('workspace_restored');
+    await fs.mkdir(destDir, { recursive: true });
+
+    for (const entry of snapshot.entries) {
+      const entryPath = path.join(destDir, entry.path);
+      console.log(entryPath);
+      if (entry.type === 'directory') {
+        await fs.mkdir(entryPath, { recursive: true });
+      } else if (entry.type === 'file') {
+        await fs.mkdir(path.dirname(entryPath), { recursive: true });
+        const content = Buffer.from(entry.content, 'base64');
+        await fs.writeFile(entryPath, content);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await restore();
