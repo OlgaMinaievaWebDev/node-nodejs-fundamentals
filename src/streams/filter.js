73861ad55eec51676680caa60c5a2281819698e1
Olga <minaeva9@gmail.com>
2026-03-08 10:57:27 -0400

    feat:solve filter.js task

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,27 @@
+import { Transform } from 'node:stream';
 const filter = () => {
   // Write your code here
   // Read from process.stdin
   // Filter lines by --pattern CLI argument
   // Use Transform Stream
   // Write to process.stdout
+
+  const args = process.argv;
+  const patternIndex = args.indexOf('--pattern');
+  const pattern = args[patternIndex + 1];
+
+  const flow = new Transform({
+    transform(chunk, encoding, callback) {
+      const lines = chunk.toString().split('\n');
+      lines.forEach((line, index) => {
+        if (line.includes(pattern)) {
+          this.push(`${line}\n`);
+        }
+      });
+      callback();
+    },
+  });
+  process.stdin.pipe(flow).pipe(process.stdout)
 };
 
 filter();
